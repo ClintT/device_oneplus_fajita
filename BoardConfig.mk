4fd7f46bfcb9d2ce605c561cb5fc83cde830f5d0
AnierinB <anierinb@evolution-x.org>
2020-07-03 05:14:08 +0000

    fajita: Transition to prebuilt TWRP BLACK
    Signed-off-by: AnierinB <anierinb@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,5 +28,8 @@ TARGET_SURFACEFLINGER_FOD_LIB := //$(DEVICE_PATH):libfod_extension.oneplus_fajit
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 
+# Prebuilt TWRP BLACK recovery
+TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := device/twrp/black/fajita/ramdisk-recovery.cpio
+
 # inherit from the proprietary version
 -include vendor/oneplus/fajita/BoardConfigVendor.mk
