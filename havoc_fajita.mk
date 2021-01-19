70724597bf2822b73ddee070478c53ceab11986e
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-19 11:56:12 +0530

    fajita: BoardConfig: Add Assert device
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- havoc_fajita.mk
+++ havoc_fajita.mk
@@ -37,10 +37,6 @@ WITH_GAPPS := true
 # FOD Animation
 EXTRA_FOD_ANIMATIONS := true
 
-# TWRP
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.product.device=OnePlus6T
-
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := havoc_fajita
 PRODUCT_DEVICE := fajita
